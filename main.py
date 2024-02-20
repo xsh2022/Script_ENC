6bb2d4a28f967cc88fbb3abb29fc43e91af98333
xsh <xishuhuan2023@163.com>
2024-02-20 15:59:31 +0800

    初始化项目

---
+++ main.py
@@ -0,0 +1,273 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import imaplib
+import json
+import os
+import signal
+import smtplib
+import time
+import email
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from imaplib import IMAP4_SSL
+
+from pyngrok import ngrok
+
+stop_signal = False
+
+DEFAULT_CONFIG_CONTENT = \
+    '''{
+  "ngrok": {
+    "ngrok_token": "1234567890",
+    "conn_type": "tcp",
+    "conn_port": 22
+  },
+  "email": {
+    "imap":{
+      "host": "imap.example.com",
+      "port": 993,
+      "username": "user@mail.example.com",
+      "password": "123456"
+    },
+    "smtp":{
+      "host": "smtp.example.com",
+      "port": 465,
+      "username": "user@mail.example.com",
+      "password": "123456"
+    }
+  }
+}
+'''
+ALLOWED_NGROK_CONN_TYPES = ['tcp', 'http']
+
+
+def read_config(path):
+    try:
+        f = open(path, 'r')
+    except FileNotFoundError:
+        print('File not found!')
+        return 1, None
+    except Exception as e:
+        print(f'Unexpected error:{e}')
+        return -1, None
+    else:
+        pass
+
+    try:
+        config_str = f.read()
+        f.close()
+        config_obj = json.loads(config_str)
+
+    except Exception as e:
+        print(f'Unexpected error:{e}')
+        return -1, '', ''
+    else:
+        pass
+    return 0, config_obj
+
+
+def create_config_file(path):
+    d, f = os.path.split(path)
+    if not os.path.isdir(d):
+        try:
+            os.makedirs(d)
+        except PermissionError:
+            print(f'Got no permission to create dir:[{d}]')
+            return 1
+        except Exception as e:
+            print(f'Unexpected error:{e}')
+            return -1
+        else:
+            pass
+    try:
+        f = open(path, 'w')
+        f.write(DEFAULT_CONFIG_CONTENT)
+        f.close()
+    except PermissionError:
+        print(f'Got no permission to write:[{path}]')
+        return 1
+    except Exception as e:
+        print(f'Unexpected error:{e}')
+        return -1
+    return 0
+
+
+def stop_handler(signum, _):
+    global stop_signal
+    print(f'----\nSignum: [{signum}]\n----')
+    stop_signal = True
+
+
+def main():
+
+    # 准备变量
+    signal.signal(signal.SIGINT, stop_handler)
+    root_dir = os.path.split(os.path.realpath(__file__))[0]
+    config_path = os.path.join(root_dir, 'config.json')
+
+    # 读取配置
+    ret, config = read_config(config_path)
+    if ret == -1:
+        return -1
+    elif ret == 1:
+        ret = create_config_file(config_path)
+        if ret != 0:
+            return ret
+        else:
+            print('Created template config file! Please fill it and restart the script.')
+            return 0
+    else:
+        pass
+    ngrok_conn_type = config['ngrok']['conn_type']
+    ngrok_conn_port = config['ngrok']['conn_port']
+    ngrok_token = config['ngrok']['ngrok_token']
+
+    imap_host = config['email']['imap']['host']
+    imap_port = config['email']['imap']['port']
+    imap_username = config['email']['imap']['username']
+    imap_password = config['email']['imap']['password']
+
+    smtp_host = config['email']['smtp']['host']
+    smtp_port = config['email']['smtp']['port']
+    smtp_username = config['email']['smtp']['username']
+    smtp_password = config['email']['smtp']['password']
+
+    if ngrok_conn_type not in ALLOWED_NGROK_CONN_TYPES:
+        print(f'[{ngrok_conn_type}] is not an allowed ngrok conn type')
+        return 2
+
+    # 设置ngrok连接
+    ngrok.set_auth_token(ngrok_token)
+
+    # IMAP 初始化
+    try:
+        imaplib.Commands['ID'] = ('AUTH',)
+        imap_conn: IMAP4_SSL = imaplib.IMAP4_SSL(host=imap_host, port=imap_port)
+    except Exception as e:
+        print(f'Unexpected error: [{e}]')
+        return -1
+    else:
+        pass
+    # IMAP连接
+    while True:
+        try:
+            imap_conn.login(imap_username, imap_password)
+            # 添加ID命令
+            ag = ("name", "automail_xsh", "contact", "automail_xsh@163.com", "version", "1.0.0",
+                    "vendor", "myclient")
+            stat, dat = imap_conn._simple_command('ID', '("' + '" "'.join(ag) + '")')
+        except imaplib.IMAP4_SSL.error as e:
+            print(f'IMAP login failure! Retrying in 5 seconds. Error:{e}')
+            time.sleep(5)
+            continue
+        except Exception as e:
+            print(f'Unexpected error: [{e}]')
+            return 1
+        else:
+            break
+
+    # NGROK连接
+    ngrok_conn = ngrok.connect(ngrok_conn_port, ngrok_conn_type)
+    print(ngrok_conn)
+
+    # 主循环
+    while True:
+
+        # 检查NGROK
+        try:
+            ngrok.get_tunnels()
+        except Exception as e:
+            if stop_signal:
+                ngrok.disconnect(ngrok_conn.public_url)
+                break
+            else:
+                print(f'Unexpected error: [{e}], restarting ngrok conn....')
+                ngrok_conn = ngrok.connect(ngrok_conn_port, ngrok_conn_type)
+        else:
+            if stop_signal:
+                ngrok.disconnect(ngrok_conn.public_url)
+                break
+
+        # IMAP读取
+        stat, data = imap_conn.select('INBOX')
+        while stat != 'OK':
+            stat, data = imap_conn.select('INBOX')
+        stat, data = imap_conn.search(None, 'UNSEEN')
+        if stat != 'OK':
+            print('Failure when reading mail')
+            time.sleep(5)
+            continue
+        email_ids = data[0].split()
+        mail_cnt = len(email_ids)
+        unseen_msg_list = []
+
+        for i in range(mail_cnt-1, -1, -1):
+            imap_conn.store(email_ids[i], '+FLAGS', '\\Seen')
+            mail_stat, mail_data = imap_conn.fetch(email_ids[i], '(RFC822)')
+            msg = email.message_from_bytes(mail_data[0][1])
+            subject_obj = email.header.decode_header(msg.get('subject'))
+            default_code = subject_obj[0][1]
+            subject_str = subject_obj[0][0]
+            if default_code is not None:
+                subject_str = subject_str.decode(default_code)
+            content_list = []
+            if msg.is_multipart():
+                pl = msg.get_payload()
+                for single_mail in pl:
+                    ctype = single_mail.get_content_type()
+                    if 'html' in ctype:
+                        html = str(single_mail.get_payload(decode=True), single_mail.get('content-type').split('=')[1])
+                    else:
+                        txt = msg.get_payload(decode=True)
+                        html = str(txt, default_code) if txt else ''
+                    content_list.append(html)
+            else:
+                txt = msg.get_payload(decode=True)
+                html = str(txt, default_code) if txt else ''
+                content_list.append(html)
+            sender: str = msg.get('from')
+            t = sender.find('<')
+            sender = sender[t+1:len(sender)-1:1]
+            unseen_msg_list.append([subject_str, sender, content_list])
+        # 处理读取的邮件、发送回复
+        for [subject, sender, body] in unseen_msg_list:
+            if subject == '获取内网穿透地址':
+                subject_back = '命令执行'
+                sender_back = smtp_username
+                receiver_back = sender
+                body_back = (f'公网地址：{ngrok_conn.public_url}\n'
+                             f'映射端口：{ngrok_conn_port}\n'
+                             f'API地址：{ngrok_conn.api_url}\n')
+            else:
+                subject_back = '已收到您的邮件'
+                sender_back = smtp_username
+                receiver_back = sender
+                body_back = '将尽快做出回复'
+            msg = MIMEMultipart()
+            msg['from'] = formataddr((Header('XSH的自动邮箱', 'utf-8').encode(), sender_back))
+            msg['to'] = receiver_back
+            msg['subject'] = Header(subject_back, 'utf-8').encode()
+            msg.attach(MIMEText(body_back, 'plain', 'utf-8'))
+            # SMTP连接
+            try:
+                smtp_conn = smtplib.SMTP_SSL(smtp_host, smtp_port)
+                smtp_conn.login(smtp_username, smtp_password)
+                smtp_conn.ehlo_or_helo_if_needed()
+                smtp_conn.sendmail(sender_back, receiver_back, msg.as_string())
+            except Exception as e:
+                print(f'Error: [{e}]')
+            else:
+                print(f'----\nMail handled, subject:[{subject}], from: [{sender}].\n'
+                      f'Back, subject: [{subject_back}], content:\n'
+                      f'{body_back}\n----')
+        time.sleep(1)
+    imap_conn.close()
+    return 0
+
+
+if __name__ == '__main__':
+    main_ret = main()
+    exit(main_ret)
---
+++ readme.md
@@ -0,0 +1,133 @@
+# EMail-NGROK 联动脚本（EMail-NGROK Co-op Script）
+
+摘要：实现了开启NGROK并通过电子邮件获取NGROK地址的功能，省去了出门在外忘记了 NGROK 地址的烦恼。
+
+Abstract: This script allow you to start NGROK while using an email to get NGROK address, which stops you from forgetting NGROK address when you are out of home.
+
+## 实现原理（Realizing）
+
+### 中文
+
+* 电子邮件收发依靠 imaplib 库和 smtplib 库
+* NGROK实现依靠 pyngrok 库
+
+### English
+
+* The email part uses imaplib and smtplib
+* The NGROK part uses pyngrok library
+
+## 使用方法（Usage）：
+
+### 中文
+
+1. 使用pip安装 pyngrok 库，注册NGROK，获取TOKEN，此外你需要注册一个邮箱（最好是163）、开启其SMTP和IMAP功能，记住口令。
+2. 第一次使用时直接打开，会在脚本的目录下创建config.json文件。
+3. 修改config.json文件中的选项，他们的命名很直观，如果你会英文那么一看就懂。不懂参照附录2
+4. 再次运行，如果没有任何异常，说明运行成功。
+
+### English
+
+1. Install the pyngrok library using pip, register for NGROK, and obtain a TOKEN. Additionally, you need to register an email address (preferably with 163.com), enable its SMTP and IMAP functions, and remember the password.
+2. When using it for the first time, simply open it, and it will create a config.json file in the script's directory.
+3. Modify the options in the config.json file; their names are intuitive, so if you understand English, you will grasp them at a glance. If not, refer to Appendix 2.
+4. Run it again; if there are no exceptions, it indicates a successful run.
+
+# 附录（Appendix）
+
+## 附录1. PIP安装PyNGROK库（Appendix 1. Install PyNGROK using PIP）
+
+### 中文
+
+打开Windows命令行或Linux、Mac-OS终端，输入：
+
+```bash
+python -m pip install pyngrok
+```
+
+### English
+
+Open cmdline of windows or terminal of linux or Mac-OS, enter:
+
+```bash
+python -m pip install pyngrok
+```
+
+## 附录2. “config.json”的参数说明（Appendix 2. Descriptions of 'config.json'）：
+
+### 中文
+
+以如下config.json文件为示例：
+
+```json
+{
+  "ngrok": {
+    "ngrok_token": "1111111111111111111111111111111111111111111111111",
+    "conn_type": "tcp",
+    "conn_port": 22
+  },
+  "email": {
+    "imap":{
+      "host": "imap.163.com",
+      "port": 993,
+      "username": "example@163.com",
+      "password": "ABCDEFGHIJKLMNOP"
+    },
+    "smtp":{
+      "host": "smtp.163.com",
+      "port": 465,
+      "username": "example@163.com",
+      "password": "ABCDEFGHIJKLMNOP"
+    }
+  }
+}
+```
+
+| 参数           | 说明                                                                                                 |
+| -------------- | ---------------------------------------------------------------------------------------------------- |
+| ngrok_token    | NGROK的TOKEN码，在官网查询                                                                           |
+| conn_type      | NGROK映射的类型，可以选择TCP、HTTP等，具体参考NGROK官网。``例如你可以选择TCP来访问ssh端口22。 |
+| conn_port      | NGROK映射的端口。                                                                                    |
+| imap->host     | IMAP的主机地址。                                                                                     |
+| imap->port     | IMAP的端口，目前只支持SSL的端口                                                                      |
+| imap->username | IMAP用户名，一般与邮箱同名                                                                           |
+| imap->password | IMAP密码，163设置页面中的“授权码”，一般与网页邮箱的登录密码不同                                    |
+| smtp->...      | 与imap->...类似                                                                                      |
+
+### English
+
+Take the following config.json for example:
+
+```json
+{
+  "ngrok": {
+    "ngrok_token": "1111111111111111111111111111111111111111111111111",
+    "conn_type": "tcp",
+    "conn_port": 22
+  },
+  "email": {
+    "imap":{
+      "host": "imap.163.com",
+      "port": 993,
+      "username": "example@163.com",
+      "password": "ABCDEFGHIJKLMNOP"
+    },
+    "smtp":{
+      "host": "smtp.163.com",
+      "port": 465,
+      "username": "example@163.com",
+      "password": "ABCDEFGHIJKLMNOP"
+    }
+  }
+}
+```
+
+| Parameter      | Description                                                                                                                                                                 |
+| -------------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| ngrok_token    | Token code for NGROK, check on the official website                                                                                                                         |
+| conn_type      | Type of mapping for NGROK, options include TCP, HTTP, etc., refer to the NGROK official website for details.``For example, you can choose TCP to access ssh port 22. |
+| conn_port      | Port for NGROK mapping                                                                                                                                                      |
+| imap->host     | Host address for IMAP                                                                                                                                                       |
+| imap->port     | Port for IMAP, currently only supports SSL port                                                                                                                             |
+| imap->username | IMAP username, usually the same as the email address                                                                                                                        |
+| imap->password | IMAP password, "authorization code" on the 163 settings page, usually different from the login password for webmail                                                         |
+| smtp->...      | Similar to imap->...                                                                                                                                                        |
